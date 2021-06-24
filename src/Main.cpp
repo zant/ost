cc59bb7854acf3d58a7492e6baf1fe2b47af09f0
gonza <gonzalo.christobal@gmail.com>
2021-06-23 22:57:35 -0400

    Test AstPrinter

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,13 +1,18 @@
+#include "AstPrinter.h"
 #include "Lox.h"
 #include <iostream>
+#include <memory>
 #include <string>
 
 int main(int argc, char *argv[]) {
-  if (argc > 2) {
-    std::cout << "Usage: jlox <script>" << std::endl;
-  } else if (argc == 2) {
-    Lox::runFile(argv[1]);
-  } else {
-    Lox::runPrompt();
-  }
+  // leaky codeeee yayyyy
+  AstPrinter().print(new Binary(new Literal(10), new Token(PLUS, "+", "", 1),
+                                new Literal(20)));
+  // if (argc > 2) {
+  //   std::cout << "Usage: jlox <script>" << std::endl;
+  // } else if (argc == 2) {
+  //   Lox::runFile(argv[1]);
+  // } else {
+  //   Lox::runPrompt();
+  // }
 }
