4d9769942d5a5ff033aed79f8322fc9eef44cee2
zant <gonzalochristobal@gmail.com>
2023-05-13 01:24:46 +0200

    refactor: use shared_ptr

--- src/Expr.h
+++ src/Expr.h
@@ -2,48 +2,55 @@
 
 #include "Token.h"
 #include <string>
+#include <fmt/core.h>
 
 // Make visitor class available
 struct Visitor;
-struct Expr {
+struct Expr
+{
   virtual std::string accept(Visitor *visitor) = 0;
+  virtual ~Expr(){};
 };
 
-struct Binary : public Expr {
-  Expr *m_Left;
-  Token *m_Op;
-  Expr *m_Right;
+struct Binary : public Expr
+{
+  std::shared_ptr<Expr> left;
+  std::shared_ptr<Token> op;
+  std::shared_ptr<Expr> right;
 
-  Binary(Expr *left, Token *op, Expr *right)
-      : m_Left(left), m_Op(op), m_Right(right){};
+  Binary(std::shared_ptr<Expr> left, std::shared_ptr<Token> op, std::shared_ptr<Expr> right)
+      : left{left}, op{op}, right{right} {};
   std::string accept(Visitor *visitor);
 };
 
-struct Unary : public Expr {
-  Expr *m_Left;
-  Token *m_Op;
+struct Unary : public Expr
+{
+  std::shared_ptr<Expr> left;
+  std::shared_ptr<Token> op;
 
-  Unary(Token *op, Expr *left) : m_Left(left), m_Op(op){};
+  Unary(std::shared_ptr<Token> op, std::shared_ptr<Expr> left) : left(left), op(op){};
   std::string accept(Visitor *visitor);
 };
 
-struct Literal : public Expr {
-  int m_Value;
+struct Literal : public Expr
+{
+  int value;
 
-  Literal(int value) : m_Value(value){};
+  Literal(int value) : value(value){};
   std::string accept(Visitor *visitor);
 };
 
-struct Grouping : public Expr {
-  Expr *m_Expr;
+struct Grouping : public Expr
+{
+  std::shared_ptr<Expr> expr;
 
-  Grouping(Expr *expr) : m_Expr(expr){};
+  Grouping(std::shared_ptr<Expr> expr) : expr(expr){};
   std::string accept(Visitor *visitor);
 };
-
 
 // Define Visitor class
-struct Visitor {
+struct Visitor
+{
   virtual std::string visitBinary(Binary *binary) = 0;
   virtual std::string visitLiteral(Literal *literal) = 0;
   virtual std::string visitGrouping(Grouping *grouping) = 0;
--- src/Main.cpp
+++ src/Main.cpp
@@ -6,9 +6,12 @@
 
 int main(int argc, char *argv[])
 {
-  Token t{TokenType::PLUS, "+", "", 1};
-  Literal l{1};
-  Binary b{&l, &t, &l};
-  auto printer = AstPrinter{};
-  printer.print(&b);
+  std::shared_ptr<Token> t{new Token{TokenType::PLUS, "+", "", 1}};
+  std::shared_ptr<Literal> l{new Literal{1}};
+  std::shared_ptr<Literal> l2{new Literal{1}};
+  Binary b{l, t, l};
+  std::shared_ptr<Binary> sb{new Binary {l, t, l}};
+  Binary b2{sb, t, l};
+  AstPrinter printer{};
+  printer.print(&b2);
 }
