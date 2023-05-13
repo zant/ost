c4eaef8790808a7383756956fab2644071e97e48
zant <gonzalochristobal@gmail.com>
2023-05-13 02:02:32 +0200

    refactor: remove unused

--- src/Main.cpp
+++ src/Main.cpp
@@ -9,9 +9,9 @@ int main(int argc, char *argv[])
   std::shared_ptr<Token> t{new Token{TokenType::Star, "*", "", 1}};
   std::shared_ptr<Literal> l{new Literal{1}};
   std::shared_ptr<Literal> l2{new Literal{1}};
-  Binary b{l, t, l};
-  std::shared_ptr<Binary> sb{new Binary {l, t, l}};
-  Binary b2{sb, t, l};
+  std::shared_ptr<Binary> sb{new Binary{l, t, l}};
+  Binary *b{new Binary(sb, t, l)};
+
   AstPrinter printer{};
-  printer.print(&b2);
+  printer.print(b);
 }
